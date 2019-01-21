cdb3b0c564c8371f2e7df2f758451b5b0453ecc2
DESKTOP-51P80NS\pc <jaltahona@eurekain.com>
2019-01-20 20:54:06 -0500

    se creo contacto funcional

--- js/script.js
+++ js/script.js
@@ -10,3 +10,35 @@ function myfunction(){
         menu.className = 'navbar navbar-expand-lg navbar-dark bg-transp fixed-top'; 
     }
 };
+
+const form = document.getElementById('contacForm'); // Obtenemos la referencia al formulario
+
+    if(form){ // Si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
+      form.addEventListener('submit', contacForm); // Al momento de enviar el formulario, ejecuta la función "contactform"
+    }
+
+    function contacForm(event) {
+      event.preventDefault(); 
+      const name = document.getElementById('name'); // Obtenemos la referencia a cada uno de nuestros elementos del formulario
+      const email = document.getElementById('email');
+      const phone = document.getElementById('phone');
+      const mensaje = document.getElementById('mensaje');
+      const data = {
+        'name': name.value,
+        'email': email.value,
+        'phone': phone.value,
+        'mensaje': mensaje.value,
+      }; // Creamos un objecto con todos los elementos de nuestro formulario.
+      savecontacForm(data); // Enviamos la información obtenida por el usuario a la función que se encargara de guardar la información en Firebase
+      form.reset(); // borramos todos los campos. 
+    }
+
+  function savecontacForm(data) {
+    firebase.database().ref('contact').push(data) // Hacemos referencia al método database de el SDK y hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
+      .then(function(){
+        alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+      })
+      .catch(function(){
+        alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
+      });
+  };
